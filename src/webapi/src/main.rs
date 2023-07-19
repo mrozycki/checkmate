f081cea0a6f19228b370d7352163042fbdaa5d8a
Mariusz Różycki <mar.rozycki@gmail.com>
2023-07-19 21:48:55 +0200

    webapi: Add tracing

--- src/webapi/src/main.rs
+++ src/webapi/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+mod routes;
+
+use actix_web::{App, HttpServer};
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
+use tracing_subscriber::{prelude::*, EnvFilter, Registry};
+
+fn initialize_tracing() {
+    LogTracer::init().expect("Failed to set logger");
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new("checkmate".into(), std::io::stdout);
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to set subscriber");
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    initialize_tracing();
+
+    HttpServer::new(|| App::new().service(routes::infra::ping))
+        .bind(("0.0.0.0", 8081))?
+        .run()
+        .await
 }
