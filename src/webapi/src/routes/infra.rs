ee23675deafe2e32628cf920ff7535059eee624f
Krzysztof Mazur <krzmazur1@gmail.com>
2023-07-27 19:33:37 +0200

    Use tracing-actix-web to automatically handle request ids

--- src/webapi/src/routes/infra.rs
+++ src/webapi/src/routes/infra.rs
@@ -1,13 +1,7 @@
 use actix_web::{get, HttpResponse, Responder};
-use uuid::Uuid;
 
 #[get("/ping")]
-#[tracing::instrument(
-    name = "Ping request", 
-    fields(
-        request_id = %Uuid::new_v4(),
-    )
-)]
+#[tracing::instrument(name = "Ping request")]
 pub(crate) async fn ping() -> impl Responder {
     HttpResponse::Ok().json("pong")
 }
