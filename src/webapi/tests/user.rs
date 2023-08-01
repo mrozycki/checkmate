36e8a76c81e166f4fbe8eb7b222c72817b3aa018
Krzysztof Mazur <krzmazur1@gmail.com>
2023-08-01 21:26:45 +0200

    Return 409 error when user already exists

--- src/webapi/tests/user.rs
+++ src/webapi/tests/user.rs
@@ -1,5 +1,6 @@
 mod common;
 
+use serde_json::json;
 use std::collections::HashMap;
 use webapi::routes;
 
@@ -8,14 +9,11 @@ async fn creating_user_returns_a_200_for_valid_data() {
     // Arrange
     let app = common::spawn_app().await;
     let client = reqwest::Client::new();
-    let mut map: HashMap<&str, &str> = HashMap::new();
-    map.insert("username", "jozin");
-    map.insert("password", "123");
 
     // Act
     let response = client
         .post(&format!("{}/user", &app.address))
-        .json(&map)
+        .json(&json!({"username": "jozin","password": "123"}))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -75,4 +73,59 @@ async fn creating_user_returns_a_400_when_data_is_missing() {
             error_message
         );
     }
+}
+
+#[tokio::test]
+async fn creating_user_returns_a_409_when_user_exists() {
+    // Arrange
+    let app = common::spawn_app().await;
+    let client = reqwest::Client::new();
+
+    // Act
+    let response1 = client
+        .post(&format!("{}/user", &app.address))
+        .json(&json!({
+            "username": "jozin",
+            "password": "123"
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    let response2 = client
+        .post(&format!("{}/user", &app.address))
+        .json(&json!({
+            "username": "jozin",
+            "password": "456"
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+
+    assert_eq!(200, response1.status().as_u16());
+    let json1 = response1
+        .json::<routes::user::CreateUserResponse>()
+        .await
+        .unwrap();
+
+    assert_eq!(
+        json1,
+        routes::user::CreateUserResponse {
+            username: "jozin".to_owned()
+        }
+    );
+
+    assert_eq!(409, response2.status().as_u16());
+    let json2 = response2.json::<serde_json::Value>().await.unwrap();
+
+    assert_eq!(json2, json!({"error": "user 'jozin' already exists"}));
+
+    let saved = sqlx::query!("SELECT username, password FROM users",)
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.username, "jozin");
+    assert_eq!(saved.password, "123");
 }
